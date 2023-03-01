d37700a434415f5c44e531beda72f1c12daeeb14
a097245 <a097245@gmail.com>
2023-03-01 10:44:49 +0800

    increase google image precision

--- background.js
+++ background.js
@@ -13,7 +13,7 @@ chrome.runtime.onMessage.addListener(
         }
 
         if (request.contentScriptQuery == 'fetchImage') {
-            let url = 'https://www.google.com/search?q='+ request.search +'&rlz=1C5CHFA_enTW772TW772&sxsrf=ALiCzsb1X5ysZDoTLy1SmBJr-6goe15jfw:1661697709572&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjy0M-t4un5AhWYAqYKHVn_DO0Q_AUoAXoECAIQAw&biw=1280&bih=812&dpr=1'
+            let url = 'https://www.google.com/search?q='+ request.search + '+meaning' +'&rlz=1C5CHFA_enTW772TW772&sxsrf=ALiCzsb1X5ysZDoTLy1SmBJr-6goe15jfw:1661697709572&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjy0M-t4un5AhWYAqYKHVn_DO0Q_AUoAXoECAIQAw&biw=1280&bih=812&dpr=1'
             fetch(url, {}).then(function (data) {
                 var a = (data.text())
                 a.then(function (value) {
